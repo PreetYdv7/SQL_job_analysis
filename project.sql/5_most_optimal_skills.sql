152ce0a89424528362b2f8f9ae305948717316a7
Preet Yadav <133802505+PreetYdv7@users.noreply.github.com>
2025-07-17 20:29:23 +0530

    Refactor SQL query to enhance clarity and performance for identifying high-demand skills and average salaries for Data Analyst roles

--- project.sql/5_most_optimal_skills.sql
+++ project.sql/5_most_optimal_skills.sql
@@ -1,43 +1,47 @@
-WITH skills_demand AS(
+-- Identifies skills in high demand for Data Analyst roles
+-- Use Query #3 (but modified)
+WITH skills_demand AS (
     SELECT
-        skills,
-        skill_id,
+        skills_dim.skill_id,
+        skills_dim.skills,
         COUNT(skills_job_dim.job_id) AS demand_count
     FROM
         job_postings_fact
         INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
         INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE
-        job_title_short = 'Data Analyst'
+        job_postings_fact.job_title_short = 'Data Analyst'
         AND job_postings_fact.salary_year_avg IS NOT NULL
+        AND job_postings_fact.job_work_from_home = True
     GROUP BY
-        skill_id,
-        skills
-    LIMIT
-        10
-), avg_salary AS (
+        skills_dim.skill_id
+),
+-- Skills with high average salaries for Data Analyst roles
+-- Use Query #4 (but modified)
+average_salary AS (
     SELECT
-        skills,
-        skill_id,
-        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS average_salary
+        skills_job_dim.skill_id,
+        AVG(job_postings_fact.salary_year_avg) AS avg_salary
     FROM
         job_postings_fact
-        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-        INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id -- There's no INNER JOIN to skills_dim because we got rid of the skills_dim.name 
     WHERE
-        job_title_short = 'Data Analyst'
+        job_postings_fact.job_title_short = 'Data Analyst'
         AND job_postings_fact.salary_year_avg IS NOT NULL
+        AND job_postings_fact.job_work_from_home = True
     GROUP BY
-        skill_id,
-        skills
-    LIMIT
-        25
-)
+        skills_job_dim.skill_id
+) -- Return high demand and high salaries for 10 skills 
 SELECT
-    skills_demand.skill_id,
     skills_demand.skills,
     skills_demand.demand_count,
-    avg_salary.average_salary
+    ROUND(average_salary.avg_salary, 2) AS avg_salary --ROUND to 2 decimals 
 FROM
-    avg_salary
-    INNER JOIN skills_demand ON avg_salary.skill_id = skills_demand.skill_id;
+    skills_demand
+    INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id -- WHERE demand_count > 10
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC
+LIMIT
+    10 --Limit 25
+;
